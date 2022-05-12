8b33735ec8b0574caf4917e8eb1125485174ad7f
ylanwb <class-computer@class-computers-MacBook-Air.local>
2022-05-11 18:43:51 -0700

    progress on exercises

--- exercise-1/array.js
+++ exercise-1/array.js
@@ -1,101 +1,152 @@
-
 // Exercise 1
-// Write a JavaScript function to sort three numbers in array. Return sorted array. 
-// Sample numbers : [0, -1, 4] 
-// Output : [4, 0, -1] 
+// Write a JavaScript function to sort three numbers in array. Return sorted array.
+// Sample numbers : [0, -1, 4]
+// Output : [4, 0, -1]
 // notes:
 //     https://www.geeksforgeeks.org/javascript-array-sort/
 //     https://www.w3schools.com/jsref/met_win_alert.asp
-
 
 // 1. write function ==> function will take array as input
 // 2.Sort the numbers inside the array and return sorted array
 
 function sortedArray(array) {
-    return array.sort(function (a,b) {
-        return b - a;
-    })
+  return array.sort(function (a, b) {
+    return b - a;
+  });
 }
 console.log(sortedArray([5, 7, 10, -10, 56])); // ==> 56, 10, 7, 5, -10
-/*
-Exercise 2
-Write a JavaScript function to find the largest of five numbers of array.  
-Sample numbers : -5, -2, -6, 0, -1 
-Output : 0 
+// Exercise 2
+// Write a JavaScript function to find the largest of five numbers of array.
+// Sample numbers : -5, -2, -6, 0, -1
+// Output : 0
 
-Exercise 3
+function largestNumber(array) {
+  return Math.max.apply(Math, array);
+}
+console.log(largestNumber([-5, -2, -6, 0, -1]));
 
-Function that will return your string in Alphabetical order
+// Exercise 3
 
-Example:
-> console.log(AlphabeticalOrder('hello'));
-> "ehllo"
+// Function that will return your string in Alphabetical order
 
-Exercise 4
+// Example:
+// > console.log(AlphabeticalOrder('hello'));
+// > "ehllo"
 
-Create a function that will display the smallest value in the array.
+function AlphabeticalOrder(string) {
+  return string.split("").sort().join("");
+}
+console.log(AlphabeticalOrder("flyday chinatown!"));
 
-Example:
-> console.log(findSmallest([30, 45, 60, 7]));
-> 1
+// Exercise 4
 
-Exercise 5
-Remove all Odd number(s) in an array and return a new array that contains Even numbers only
+// Create a function that will display the smallest value in the array.
 
-Example:
-> console.log(evenOnly([1, 2, 3, 4, 5, 6]));
-> [ 2, 4, 6 ]
+// Example:
+// > console.log(findSmallest([30, 45, 60, 7]));
+// > 1
 
-Exercise 6 
+function findSmallest(array) {
+  return Math.min.apply(Math, array);
+}
+console.log(findSmallest([30, 45, 60, 7]));
 
-Create a function that will accept an array, check the data type of each element. The function will delete string elements and will return a the new array
+// Exercise 5
+// Remove all Odd number(s) in an array and return a new array that contains Even numbers only
 
-Example:
-> console.log(numbersOnly(['text', 3, 7, 'github', 13, 'dev']));
-> [ 3, 7, 13 ]
+// Example:
+// > console.log(evenOnly([1, 2, 3, 4, 5, 6]));
+// > [ 2, 4, 6 ]
 
-Exercise 7
+function evenOnly(array) {
+  return array.filter((x) => x % 2 === 0);
+}
+console.log(evenOnly([1, 2, 3, 4, 5, 6]));
 
-Create a function that will accept an array and do the following:
-Get the lowest element
-Get the highest element
-Get the length of array
-Get the Average of all element;
-Store these criteria in a new array
+// Exercise 6
 
-Example:
-> console.log(minMaxLengthAverage([7, 13, 3, 77, 100]));
-> [ 3, 100, 5, 40 ]
+// Create a function that will accept an array, check the data type of each element. The function will delete string elements and will return a the new array
 
-Exercise 8 
+// Example:
+// > console.log(numbersOnly(['text', 3, 7, 'github', 13, 'dev']));
+// > [ 3, 7, 13 ]
+function numbersOnly(array) {
+  return array.filter((string) => typeof string === "number");
+}
+console.log(numbersOnly(["text", 3, 7, "github", 13, "dev"]));
 
-Count the number of Words
-Return how many words was given
+// Exercise 7
 
-Example:
-> countWords('hello from kbpsystem!');
-> 3
+// Create a function that will accept an array and do the following:
+// Get the lowest element
+// Get the highest element
+// Get the length of array
+// Get the Average of all element;
+// Store these criteria in a new array
 
-Exercise 9 
+// Example:
+// > console.log(minMaxLengthAverage([7, 13, 3, 77, 100]));
+// > [ 3, 100, 5, 40 ]
 
-Multiply by Length
-Multiply all elements in an array by it's length
+function minMaxLengthAverage(array) {
+  let length = array.length;
+  let smallest = Math.min.apply(Math, array);
+  let biggest = Math.max.apply(Math, array);
+  let sum = length + smallest + biggest;
+  let average = sum / length;
+  return average;
+}
+console.log(minMaxLengthAverage([3, 100, 5, 40]));
 
-Example:
-> MultiplyByLength([4,1,1]);
-> [12, 3, 3]
+// Exercise 8
 
-Exercise 10
+// Count the number of Words
+// Return how many words was given
 
-Find the correct Index location
-Return the index location of an element from a given array. First argument is the array you'd like to search and the second one is the element (either string/number) to look for.
+// Example:
+// > countWords('hello from kbpsystem!');
+// > 3
 
-Example:
-> console.log(findIndex(['github', 'gitlab', 'bitbucket', 'apollo'], 'gitlab'));
-> 1
+function countWords(string) {
+  let words = string.split(" ").length;
+  return words;
+}
+console.log(countWords("You Got Jokes"));
 
-*/
+// Exercise 9
 
+// Multiply by Length
+// Multiply all elements in an array by it's length
+
+// Example:
+// > MultiplyByLength([4,1,1]);
+// > [12, 3, 3]
+
+function MultiplyByLength(array) {
+  let length = array.length;
+  let numbers = array.map(function (x) {
+    return x * length;
+  });
+  return numbers;
+}
+console.log(MultiplyByLength([12, 3, 3]));
+
+// Exercise 10
+
+// Find the correct Index location
+// Return the index location of an element from a given array.
+// First argument is the array you'd like to search and the second one is the element (either string/number) to look for.
+
+// Example:
+// > console.log(findIndex(['github', 'gitlab', 'bitbucket', 'apollo'], 'gitlab'));
+// > 1
+
+function findIndex(array, string) {
+    return array.indexOf(string)
+}
+console.log(
+  findIndex(["github", "bitbucket", "gitlab", "bitbucket", "apollo"], "gitlab")
+);
 
 // const array = [1, 2, 3, 4, 5];
 // const newArray = array.map(function (element) {
--- exercise-1/array2.js
+++ exercise-1/array2.js
@@ -66,19 +66,41 @@ const zooAnimals = [
 ];
 
 /* Request 1: .forEach()
-The zoos want to display both the scientific name and the animal name in front of the habitats. Populate the displayNames array with only the animal_name and scientific_name of each animal. displayNames will be an array of strings, and each string should follow this pattern: "Name: Jackal, asiatic, Scientific: Canis aureus."
+The zoos want to display both the scientific name and the animal name 
+in front of the habitats. Populate the displayNames array with only the
+ animal_name and scientific_name of each animal. 
+ displayNames will be an array of strings, and each string 
+ should follow this pattern: "Name: Jackal, asiatic, Scientific: Canis aureus."
 */
 
+function nameFunction(array) {
+  return array.map((arr) => {
+    return `Name: ${arr.animal_name}, asiatic, Scientific: ${arr.scientific_name}.`;
+  });
+}
+console.log(nameFunction(zooAnimals));
 /* Request 2: .map()
-The zoos need a list of all their animal's names (animal_name only) converted to lower case. Using map, create a new array of strings named lowCaseAnimalNames, each string following this pattern: "jackal, asiatic". Log the resut.
+The zoos need a list of all their animal's names (animal_name only) converted to lower case.
+ Using map, create a new array of strings named lowCaseAnimalNames,
+  each string following this pattern: "jackal, asiatic". Log the resut.
 */
-
+function lowerCase(array) {
+  return array.map((arr) => {
+    let string = arr.animal_name.split(' ').toString().toLowerCase();
+    return string.split('').join('')
+  })
+}
+console.log(lowerCase(zooAnimals))
 /* Request 3: .filter() 
-The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
+The zoos are concerned about animals with a lower population count. Using filter, create a new 
+array of objects called lowPopulationAnimals which contains only the animals with a population 
+less than 5.
 */
 
 /* Request 4: .reduce() 
-The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
+The zoos need to know their total animal population across the United States. Find the total 
+population from all the zoos using the .reduce() method. Remember the reduce method takes two 
+arguments: a callback (which itself takes two args), and an initial value for the count.
 */
 
 // ==== Callbacks ====
@@ -93,7 +115,8 @@ The zoos need to know their total animal population across the United States. Fi
 /* Step 2: Create several functions to callback with consume();
  * Create a function named add that returns the sum of two numbers
  * Create a function named multiply that returns the product of two numbers
- * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
+ * Create a function named greeting that accepts a first and last name and returns "Hello
+ *  first-name last-name, nice to meet you!"
  */
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
