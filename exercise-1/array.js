27c6b393c04f0d23a83a7dd612cbc9e009b2dc0a
Tyler <tyler@class-computers-MacBook-Air.local>
2022-05-22 14:01:56 -0700

    exercise

--- exercise-1/array.js
+++ exercise-1/array.js
@@ -44,12 +44,10 @@ console.log(getLargest([1, 3, 56, 52347, 365]));
 //> console.log(AlphabeticalOrder('hello'));
 //> "ehllo"
 // function abcOrder
-function alphabet_order(str)
-  {
-return str.split('').sort().join('');
-  }
+function alphabet_order(str) {
+  return str.split("").sort().join("");
+}
 console.log(alphabet_order("hello"));
-
 
 //Exercise 4
 
@@ -58,9 +56,9 @@ console.log(alphabet_order("hello"));
 //Example:
 //> console.log(findSmallest([30, 45, 60, 7]));
 //> 1
-const findSmallest = [14, 58, 20, 77, 66, 82, 42, 67, 42, 4]
-const min = findSmallest.reduce((a, b) => Math.min(a, b))
-console.log(min)
+const findSmallest = [14, 58, 20, 77, 66, 82, 42, 67, 42, 2, 4];
+const min = findSmallest.reduce((a, b) => Math.min(a, b));
+console.log(min);
 
 //Exercise 5
 //Remove all Odd number(s) in an array and return a new array that contains Even numbers only
@@ -68,31 +66,54 @@ console.log(min)
 //Example:
 // > console.log(evenOnly([1, 2, 3, 4, 5, 6]));
 // > [ 2, 4, 6 ]
-function findOdd(A) {
-  let counts = A.reduce((p, n) => (p[n] = ++p[n] || 1, p), {});
-  return +Object.keys(counts).find(k => counts[k] % 2) || undefined;
+let array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+let odds = array1.filter((n) => n % 2 === 0);
+
+console.log(odds);
+
+// Exercise 6
+
+// Create a function that will accept an array, check the data type of each element.
+// The function will delete string elements and will
+// return a the new array
+//notes
+// you are gonna cost array, you make an array of anything like numbers string boolian and object
+//then const a name ex: filteredArray then we are gonna use the name we const the name  above
+// and add .filter that takes element => then return the type of element
+//that is not equal to string, after than console log filteredArray
+const array = ["hihi", 5, { name: "Hi" }];
+const filteredArray = array.filter((element) => {
+  return typeof element !== "string";
+});
+console.log(filteredArray);
+
+// Example:
+// > console.log(numbersOnly(['text', 3, 7, 'github', 13, 'dev']));
+// > [ 3, 7, 13 ]
+
+// Exercise 7
+
+// Create a function that will accept an array and do the following:
+// Get the lowest element
+// Get the highest element
+// Get the length of array
+// Get the Average of all element;
+// Store these criteria in a new array
+
+// Example:
+// > console.log(minMaxLengthAverage());
+// > [ 3, 100, 5, 40 ]
+const arr = [7, 13, 3, 77, 100];
+function minMaxLengthAverage(array) {
+  const min = array.reduce((a, b) => Math.min(a, b));
+  const highest = Math.max(array);
+  const length = arr.length(array);
+  const average
 }
+console.log(minMaxLengthAverage(arr));
+
 /*
-Exercise 6 
-
-Create a function that will accept an array, check the data type of each element. The function will delete string elements and will return a the new array
-
-Example:
-> console.log(numbersOnly(['text', 3, 7, 'github', 13, 'dev']));
-> [ 3, 7, 13 ]
-
-Exercise 7
-
-Create a function that will accept an array and do the following:
-Get the lowest element
-Get the highest element
-Get the length of array
-Get the Average of all element;
-Store these criteria in a new array
-
-Example:
-> console.log(minMaxLengthAverage([7, 13, 3, 77, 100]));
-> [ 3, 100, 5, 40 ]
 
 Exercise 8 
 
